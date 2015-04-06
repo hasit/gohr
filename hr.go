3cb8ec4343acc7e44fccaed274495ea3752da01f
hasit <hasit@uw.edu>
2015-04-06 10:49:58 -0700

    comments added

--- hr.go
+++ hr.go
@@ -1,3 +1,4 @@
+//make it available as a command
 package main
 
 import (
@@ -7,6 +8,7 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+//get number of columns of terminal from crypto subdirectory ssh/terminal
 func getCols() int {
 	c, _, err := terminal.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
@@ -15,6 +17,7 @@ func getCols() int {
 	return c
 }
 
+//fill a row with '#' by default (if no arguments are provided) or take command line arguments and print each pattern on a new line
 func drawHr(args []string) {
 	cols := getCols()
 
@@ -29,11 +32,12 @@ func drawHr(args []string) {
 			for i := 0; i < cols/l; i++ {
 				fmt.Printf(arg)
 			}
-			fmt.Printf("%v\n", arg[:cols%l])
+			fmt.Printf("%v\n", arg[:cols%l]) //this fills ups the remaining columns in the row with part of the pattern
 		}
 	}
 }
 
+//get command line arguments from 1 onwards (exclude the file name)
 func getArgs() {
 	args := os.Args[1:]
 	drawHr(args)
