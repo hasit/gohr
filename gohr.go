67d792b9039bf57f47368d3edb9816971eff7ee1
hasit <hasit@gmail.com>
2017-06-13 15:17:44 -0700

    Return appropriate error from getWidth()

--- gohr.go
+++ gohr.go
@@ -2,37 +2,41 @@ package gohr
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-//get number of columns of terminal from crypto subdirectory ssh/terminal
-func getCols() int {
-	c, _, err := terminal.GetSize(int(os.Stdout.Fd()))
+// getWidth gets number of width of terminal from crypto subdirectory ssh/terminal
+func getWidth() (int, error) {
+	w, _, err := terminal.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
-		panic(err)
+		return -1, err
 	}
-	return c
+	return w, nil
 }
 
-// DrawHr fills a row with '#' by default (if no arguments are provided) or take command line arguments and print each pattern on a new line
-func DrawHr(args ...string) {
-	cols := getCols()
+// Draw fills a row with '#' by default (if no arguments are provided) or takes arguments and prints each pattern on a new line.
+func Draw(args ...string) {
+	w, err := getWidth()
+	if err != nil {
+		log.Fatalf("Error getting terminal width: %s\n", err)
+	}
 
 	if len(args) == 0 {
-		for i := 0; i < cols; i++ {
+		for i := 0; i < w; i++ {
 			fmt.Printf("#")
 		}
-		fmt.Println()
+		fmt.Printf("\n")
 	} else {
 		for _, arg := range args {
 			l := len(arg)
-			for i := 0; i < cols/l; i++ {
+			for i := 0; i < w/l; i++ {
 				fmt.Printf(arg)
 			}
-			// Fill ups the remaining columns in the row with part of the pattern
-			fmt.Printf("%v\n", arg[:cols%l])
+			// Fills up the remaining columns in the row with part of the pattern
+			fmt.Printf("%s\n", arg[:w%l])
 		}
 	}
 }
