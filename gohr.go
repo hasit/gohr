b7cad355cbdbf13146a077500a2cda9d93713c45
hasit <hasit@gmail.com>
2017-06-14 15:47:15 -0700

    Separate pattern drawing logic out of Draw(patterns ...string)

--- gohr.go
+++ gohr.go
@@ -2,6 +2,7 @@ package gohr
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -9,7 +10,7 @@ import (
 )
 
 // getWidth gets number of width of terminal from crypto subdirectory ssh/terminal
-func getTerminalWidth() (int, error) {
+func getWidth() (int, error) {
 	w, _, err := terminal.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
 		return -1, err
@@ -17,26 +18,36 @@ func getTerminalWidth() (int, error) {
 	return w, nil
 }
 
-// Draw fills a row with '#' by default (if no arguments are provided) or takes arguments and prints each pattern on a new line.
-func Draw(patterns ...string) {
-	w, err := getTerminalWidth()
-	if err != nil {
-		log.Fatalf("Error getting terminal width: %s\n", err)
-	}
-
+func drawPatterns(width int, patterns []string, wr io.Writer) {
 	if len(patterns) == 0 {
-		for i := 0; i < w; i++ {
+		for i := 0; i < width; i++ {
 			fmt.Printf("#")
 		}
 		fmt.Printf("\n")
 	} else {
 		for _, pattern := range patterns {
-			l := len(pattern)
-			for i := 0; i < w/l; i++ {
+			patternLength := len(pattern)
+			for i := 0; i < width/patternLength; i++ {
 				fmt.Printf(pattern)
 			}
 			// Fills up the remaining columns in the row with part of the pattern
-			fmt.Printf("%s\n", pattern[:w%l])
+			fmt.Printf("%s\n", pattern[:width%patternLength])
 		}
 	}
+}
+
+// Draw fills a row with '#' by default (if no arguments are provided) or takes arguments and prints each pattern on a new line.
+func Draw(patterns ...string) {
+	w, err := getWidth()
+	if err != nil {
+		log.Fatalf("Error getting terminal width: %s\n", err)
+	}
+
+	var p []string
+
+	for _, pattern := range patterns {
+		p = append(p, pattern)
+	}
+
+	drawPatterns(w, p, os.Stdout)
 }
